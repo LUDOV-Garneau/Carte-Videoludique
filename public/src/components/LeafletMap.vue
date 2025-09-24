a4b3784cbe0909e2098b25ef486912eace712c5b
Jasmin Martel <jamar003@edu.cegepgarneau.ca>
2025-09-24 00:28:12 -0400

    WIP - Ajout d'un bouton d'ajout de marqueur dans la map

--- public/src/components/LeafletMap.vue
+++ public/src/components/LeafletMap.vue
@@ -21,6 +21,7 @@ L.Marker.prototype.options.icon = DefaultIcon
 
 const mapEl = ref(null)
 let map
+let controlAjoutMarqueur
 
 onMounted(() => {
   // Centre par défaut (Montréal) — ajuste selon ton besoin
@@ -41,21 +42,52 @@ onMounted(() => {
       .addTo(map)
       .bindPopup(`Proposition<br>${lat.toFixed(5)}, ${lng.toFixed(5)}`)
   })
+
+  const ControlAjoutMarqueur = L.Control.extend({
+    options : { position: 'topright'},
+    onAdd: function () {
+      const container = L.DomUtil.create('div', 'leaflet-control leaflet-control-custom')
+      
+      const btn = L.DomUtil.create('a', 'btn-ajout-marqueur', container)
+      btn.href = '#'
+      btn.title = 'Ajouter un marqueur'
+      btn.textContent = 'Ajouter un marqueur'
+      btn.setAttribute('role', 'button')
+      btn.setAttribute('aria-label', 'Ajouter un marqueur')
+      // btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>'
+      btn.innerHTML = '<span aria-hidden="true">+</span><span class="sr-only">Ajouter un marqueur</span>';
+
+      L.DomEvent.disableClickPropagation(container)
+      L.DomEvent.disableScrollPropagation(container)
+
+      L.DomEvent.on(btn, 'click', (e) => {
+        L.DomEvent.preventDefault(e)
+        console.log('Clique sur le bouton affiche le formulaire d’ajout de marqueur')
+      })
+      return container
+    }
+  })
+  controlAjoutMarqueur = new ControlAjoutMarqueur()
+  map.addControl(controlAjoutMarqueur)
 })
 
 onUnmounted(() => {
-  if (map) map.remove()
+  if (map) {
+    if (controlAjoutMarqueur) map.removeControl(controlAjoutMarqueur)
+    map.remove()
+  }
 })
 </script>
 
 <template>
-  <div class="map-container" ref="mapEl" />
+  <div class="map" ref="mapEl" />
 </template>
 
-<style scoped>
-.map-container {
+<style>
+.map {
   width: 100%;
   /* 100vh si tu veux plein écran, sinon fixe une hauteur : */
   height: 100vh;
 }
+
 </style>
