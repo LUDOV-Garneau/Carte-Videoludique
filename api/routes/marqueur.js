0893885c455071e5049b637e7f6278f1ddb55703
FrederickBlouin <frederick.blouin.5@gmail.com>
2025-09-25 09:02:30 -0400

    Correction de manrqueur.js

---
+++ api/routes/marqueur.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const express = require("express");
+
+const router = express.Router();
+
+const marqueurController = require("../controllers/marqueurController");
+
+const isAuth = require("../middleware/isAuth");
+
+
+// POST => /marqueurs
+router.post("/marqueurs", marqueurController.createMarqueur);
+
+// GET => /marqueurs 
+router.get("/marqueurs", marqueurController.getMarqueurs);
+
+// GET => /marqueurs/:marqueurId
+router.get("/marqueurs/:marqueurId", marqueurController.getMarqueur);
+
+// PUT => /marqueurs/:marqueurId
+router.put("/marqueurs/:marqueurId", isAuth, marqueurController.updateMarqueur);
+
+// DELETE => /marqueurs/:marqueurId
+router.delete("/marqueurs/:marqueurId", isAuth, marqueurController.deleteMarqueur);
+
+module.exports = router;
