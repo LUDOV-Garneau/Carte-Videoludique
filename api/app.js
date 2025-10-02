9bd7ea9911ffa801becfb55d1771bb8f789c1491
JasminM <jamar003@edu.cegepgarneau.ca>
2025-10-02 10:20:12 -0400

    fix 3

--- api/app.js
+++ api/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'https://ludov-garneau.github.io');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	if (req.method === 'OPTIONS') {
