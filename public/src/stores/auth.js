d3ce83d2568458f859213b88507b104e3fd1ee32
Tsoungui Manuela <auriginart@gmail.com>
2025-10-02 09:35:56 -0400

    Store Pinia

---
+++ public/src/stores/auth.js
@@ -0,0 +1,59 @@
+import { defineStore,acceptHMRUpdate } from 'pinia'
+import { jwtDecode } from "jwt-decode";
+import { ref, computed } from 'vue'
+
+
+export const useAuthStore = defineStore('auth', () => {
+  
+  //récupérer le token depuis localStorage si on l'utilise
+  const token = ref(localStorage.getItem('jwt') || null);
+
+  function setToken(newToken) {
+  token.value = newToken
+  localStorage.setItem('jwt', newToken)
+}
+ 
+  // Décode les infos utilisateur à partir du token
+  const decodedToken = computed(() => {
+    if (token.value) {
+      try {
+        return jwtDecode(token.value)
+      } catch (e) {
+        return null
+      }
+    }
+    return null
+  })
+// Obtenir le nom d'utilisateur depuis le token décodé
+  const username = computed(() => {
+    return decodedToken.value ? decodedToken.value.nom : '';
+  });
+  
+const isAuthenticated = computed(() => {
+  const decoded = decodedToken.value
+  if (!decoded) return false
+
+  const now = Date.now() / 1000
+  return decoded.exp >= now
+})
+
+  function logout() {
+    token.value = null
+    // Supprimer le token de localStorage
+    localStorage.removeItem('jwt'); // Supprimer le token de localStorage
+    
+    
+  }
+
+  return {
+    token,
+    username,
+    decodedToken,
+    isAuthenticated,
+    logout,
+    setToken
+  }
+})
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot));
+}
