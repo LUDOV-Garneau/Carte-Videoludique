ddbc96b758b7a78553013dd3847f5bc0d49eb947
Broskie67 <127433699+Broskie67@users.noreply.github.com>
2025-10-05 14:24:06 -0400

    fix redirection connexion a adminview

--- api/app.js
+++ api/app.js
@@ -1,20 +1,15 @@
 "use strict";
 
-//const hateoasLinker = require("express-hateoas-links");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-// const cors = require('cors');
+const cors = require('cors');
 
-// app.use(cors());
+app.use(cors());
 
-
-
-// parse application/json
-// Permet de parser le corps des requêtes (body) en JSON
 app.use(express.json());  
 
 
@@ -26,46 +21,38 @@ app.use((req, res, next) => {
 	if (req.method === 'OPTIONS') {
 		return res.sendStatus(200).end();
 	}
-
-  next();
+	next();
 });
-
-
-
-// Importe les routes
 
 const adminRoutes = require("./routes/admin");
 const marqueurRoutes = require("./routes/marqueur")
-// const tableauRoutes = require("./routes/tableau");
-// const listeRoutes = require("./routes/liste");
-// const carteRoutes = require("./routes/carte");
-// const seed = require("./routes/db");
 
-// Utilisation des routes en tant que middleware
 // app.use(seed);
 
 app.use(adminRoutes);
 app.use(marqueurRoutes);
-// app.use(tableauRoutes);
-// app.use(listeRoutes);
-// app.use(carteRoutes);
 
-// remplace le res.json standard avec la nouvelle version
-// qui prend en charge les liens HATEOAS
-//app.use(hateoasLinker);
+app.use((err, req, res, next) => {
+  console.error('Unhandled error:', err);
+  const code = err.status || 500;
+  res.status(code).json({ message: err.message || 'Erreur serveur.' });
+});
 
 console.log("DATA_BASE =", process.env.DATA_BASE);
 
-
-// Démarrage du serveur
-(async () => {
-	try {
-		await mongoose.connect(process.env.DATA_BASE);
+// (async () => {
+// 	try {
+// 		await mongoose.connect(process.env.DATA_BASE);
   
-		app.listen(process.env.PORT, () => {
-			console.log(`Serveur à l'écoute sur : http://localhost:${process.env.PORT}`);
-		});
-	} catch (err) {
-		console.error("Erreur de connexion à MongoDB :", err);
-	}
-  })();
+// 		app.listen(process.env.PORT, () => {
+// 			console.log(`Serveur à l'écoute sur : http://localhost:${process.env.PORT}`);
+// 		});
+// 	} catch (err) {
+// 		console.error("Erreur de connexion à MongoDB :", err);
+// 	}
+//   })();
+mongoose.connect(process.env.DATA_BASE)
+  .then(() => console.log("MongoDB connecté"))
+  .catch((err) => console.error("Erreur MongoDB :", err));
+
+module.exports = app;
