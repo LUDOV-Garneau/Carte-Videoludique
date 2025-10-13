5feed35acb4c294d903149b6cdd10b29b5397895
Jasmin Martel <jamar003@edu.cegepgarneau.ca>
2025-10-12 23:14:23 -0400

    WIP - Ajout des tuiles d'images

--- public/src/components/AddImage.vue
+++ public/src/components/AddImage.vue
@@ -3,7 +3,7 @@ import { ref, reactive, onBeforeMount } from 'vue';
 
 const images = reactive([]);
 
-function trigger() {
+function pick() {
 
 }
 function onPick(event) {
@@ -13,7 +13,7 @@ function onPick(event) {
 <template>
     <div class="add-image">
         <!-- Aucune image -->
-        <button v-if="images.length === 0" type="button" class="add-btn" @click="trigger()">
+        <button v-if="images.length === 0" type="button" class="add-btn" @click="pick()">
             <svg class="camera-icon" width="18" height="18" viewBox="0 0 24 24" aria-hidden="true">
                 <path fill="currentColor"
                     d="M7.5 6h2.1l1.2-1.6c.28-.37.71-.6 1.18-.6h2.24c.47 0 .9.23 1.18.6L16.6 6H19a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h2.5zm4.5 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10zm0-2.2a2.8 2.8 0 1 1 0-5.6 2.8 2.8 0 0 1 0 5.6z"/>
@@ -24,18 +24,26 @@ function onPick(event) {
 
         <!-- Avec images -->
         <div v-else class="tiles">
-            <button type="button" class="tile add-btn-tile" @click="trigger()">
+            <!-- tuile d'ajout -->
+            <button type="button" class="tile add-btn-tile" @click="pick()">
                 <svg class="camera-icon" width="18" height="18" viewBox="0 0 24 24" aria-hidden="true">
                     <path fill="currentColor"
                         d="M7.5 6h2.1l1.2-1.6c.28-.37.71-.6 1.18-.6h2.24c.47 0 .9.23 1.18.6L16.6 6H19a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h2.5zm4.5 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10zm0-2.2a2.8 2.8 0 1 1 0-5.6 2.8 2.8 0 0 1 0 5.6z"/>
                     <path fill="currentColor" d="M20.5 6.5h-1v-1a.75.75 0 1 0-1.5 0v1h-1a.75.75 0 1 0 0 1.5h1v1a.75.75 0 1 0 1.5 0v-1h1a.75.75 0 0 0 0-1.5z"/>
                 </svg>
             </button>
+
+            <!-- tuiles d'images -->
+            <div v-for="(image, index) in images" :key="image.id" class="tile thumbnail" @click="open(index)">
+                <img :src="image.url" :alt="'Image ' + (index + 1)" />
+                <button type="button" class="delete-btn" @click.stop="removeAt(index)" aria-label="Supprimer">✕</button>
+                
+            </div>
         </div>
 
-        <input type="file" class="hidden" accept="image/*" multiple @change="onPick" />
+        <input type="file" class="hidden" accept="image/*" multiple @change="onPick()" />
 
-        
+
     </div>
 </template>
 <style scoped>
