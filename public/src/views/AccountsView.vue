b8824eb3a1e26cc78934beccc69c6d568a23e0e9
Tsoungui Manuela <auriginart@gmail.com>
2025-10-23 04:51:52 -0400

    Accounts view

---
+++ public/src/views/AccountsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Profil</h1>
+    <p>Ici s'affichera le profil de l'utilisateur.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileView",
+};
+</script>
