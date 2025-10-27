ddb1adc8d795a9ae7347f6f828fa4aa2de104e0b
FrederickBlouin <frederick.blouin.5@gmail.com>
2025-10-27 08:29:33 -0400

    Fix - Changement du modèle du marqueur

--- api/app.js
+++ api/app.js
@@ -3,56 +3,52 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
-
-app.use(express.json());  
-
+app.use(express.json());
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-
-	res.setHeader('Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	if (req.method === 'OPTIONS') {
-		return res.sendStatus(200).end();
-	}
-	next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200).end();
+  }
+  next();
 });
 
 const adminRoutes = require("./routes/admin");
-const marqueurRoutes = require("./routes/marqueur")
-
-// app.use(seed);
+const marqueurRoutes = require("./routes/marqueur");
 
 app.use(adminRoutes);
 app.use(marqueurRoutes);
 
 app.use((err, req, res, next) => {
-  console.error('Unhandled error:', err);
+  console.error("Unhandled error:", err);
   const code = err.status || 500;
-  res.status(code).json({ message: err.message || 'Erreur serveur.' });
+  res.status(code).json({ message: err.message || "Erreur serveur." });
 });
 
 console.log("DATA_BASE =", process.env.DATA_BASE);
 
-// (async () => {
-// 	try {
-// 		await mongoose.connect(process.env.DATA_BASE);
-  
-// 		app.listen(process.env.PORT, () => {
-// 			console.log(`Serveur à l'écoute sur : http://localhost:${process.env.PORT}`);
-// 		});
-// 	} catch (err) {
-// 		console.error("Erreur de connexion à MongoDB :", err);
-// 	}
-//   })();
-mongoose.connect(process.env.DATA_BASE)
-  .then(() => console.log("MongoDB connecté"))
-  .catch((err) => console.error("Erreur MongoDB :", err));
+// Connexion MongoDB + lancement serveur
+mongoose
+  .connect(process.env.DATA_BASE)
+  .then(() => {
+    console.log("✅ MongoDB connecté");
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`🚀 Serveur à l'écoute sur : http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error("❌ Erreur MongoDB :", err));
 
 module.exports = app;
