591fb2f1506ba9314c49e6eae7b330bd8f94d629
Jasmin Martel <jasminouatr@gmail.com>
2025-11-12 19:06:02 -0500

    Ajout des coordonnées dans la liste

--- public/src/components/MarqueurPanel.vue
+++ public/src/components/MarqueurPanel.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, computed } from 'vue';
 import { useMarqueurStore } from '../stores/useMarqueur';
+import { svg } from 'leaflet';
 
 // props and emits
 const props = defineProps({
@@ -39,17 +40,29 @@ function copyToClipboard(text) {
 				<p>Catégorie : {{ marqueurStore.marqueurActif.properties.type }}</p>
             </header>
             <div class="panel__body">
-				<div id="description">
+				<div class="panel__section">
 					<h5>Description</h5>
 					<p>{{ marqueurStore.marqueurActif.properties.description }}</p>
+					<span>Créé par : {{ marqueurStore.marqueurActif.properties.createdByName }}</span>
 				</div>
-				<div id="info-list">
+				<div class="panel__info-list">
 					<div v-if="marqueurStore.marqueurActif.properties.adresse" class="info-item">
 						<svg class="info-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="currentColor"/>
 						</svg>
 						<span class="info-text">{{ marqueurStore.marqueurActif.properties.adresse }}</span>
 						<button class="info-copy-button" @click="copyToClipboard(marqueurStore.marqueurActif.properties.adresse)" title="Copier l'adresse">
+							<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M16 1H4C2.9 1 2 1.9 2 3v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" fill="currentColor"/>
+							</svg>
+						</button>
+					</div>
+					<div v-if="marqueurStore.marqueurActif.geometry.coordinates" class="info-item">
+						<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none " class="info-icon">
+							<path fill="currentColor" stroke="currentColor" stroke-width="2" d="M12,10 C14.209139,10 16,8.209139 16,6 C16,3.790861 14.209139,2 12,2 C9.790861,2 8,3.790861 8,6 C8,8.209139 9.790861,10 12,10 Z M12,10 L12,22"></path>
+						</svg>
+						<span class="info-text">Coordonnées : {{ marqueurStore.marqueurActif.geometry.coordinates.join(', ') }}</span>
+						<button class="info-copy-button" @click="copyToClipboard(marqueurStore.marqueurActif.geometry.coordinates.join(', '))" title="Copier les coordonnées">
 							<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path d="M16 1H4C2.9 1 2 1.9 2 3v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" fill="currentColor"/>
 							</svg>
@@ -133,21 +146,27 @@ function copyToClipboard(text) {
 	border-top: gray 1px solid;
 }
 
-#description {
-	padding: 12px 12px 0 12px;
+.panel__section {
+	padding: 12px;
+}
+.panel__section p {
+	margin: 0;
+}
+.panel__section span {
+	font-size: 14px;
+	text-decoration: underline;
 }
 
 /* ---------- Info list ---------- */
-#info-list {
+.panel__info-list {
 	
 }
-
 .info-item {
 	display: flex;
 	align-items: center;
 	width: 100%;
 	margin: 0;
-	padding: 8px 12px;
+	padding: 8px 12px 8px 0;
 	position: relative;
 	transition: background-color 0.2s ease;
 	cursor: default;
@@ -155,22 +174,19 @@ function copyToClipboard(text) {
 .info-item:hover {
 	background: rgba(76, 175, 80, 0.1);
 }
-
 .info-icon {
 	width: 20px;
 	height: 20px;
-	margin: 0 12px 0 12px;
+	margin: 0 12px;
 	flex-shrink: 0;
 	color: #4CAF50;
 }
-
 .info-text {
 	font-size: 14px;
 	color: #333;
 	line-height: 1.4;
 	flex: 1;
 }
-
 .info-copy-button {
 	width: 24px;
 	height: 24px;
@@ -188,7 +204,6 @@ function copyToClipboard(text) {
 	right: 12px;
 	z-index: 2;
 }
-
 .info-item::after {
 	content: '';
 	position: absolute;
@@ -201,19 +216,15 @@ function copyToClipboard(text) {
 	opacity: 0;
 	z-index: 1;
 }
-
 .info-item:hover .info-copy-button {
 	opacity: 1;
 }
-
 .info-item:hover::after {
 	opacity: 1;
 }
-
 .info-copy-button:hover {
 	color: #4CAF50;
 }
-
 .info-copy-button svg {
 	width: 100%;
 	height: 100%;
