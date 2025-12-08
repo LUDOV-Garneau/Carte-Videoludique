1d6d038dfd990f278bb549c0170e9d79917dfbd2
FrederickBlouin <frederick.blouin.5@gmail.com>
2025-12-08 08:22:36 -0500

    Correction des routes de marqueur

--- api/routes/marqueur.js
+++ api/routes/marqueur.js
@@ -10,7 +10,6 @@ const editRequestController = require("../controllers/editRequestController");
 const isAuth = require("../middlewares/isAuth");
 const optionalAuth = require("../middlewares/optionalAuth");
 
-
 // POST => /marqueurs
 router.post("/marqueurs", optionalAuth, marqueurController.createMarqueur);
 
@@ -26,6 +25,11 @@ router.put("/marqueurs/:marqueurId", isAuth, marqueurController.updateMarqueur);
 // PUT => /marqueurs/:marqueurId/status
 router.put("/marqueurs/:marqueurId/status", isAuth, marqueurController.updateStatusMarqueur);
 
+// --- ARCHIVAGE / RESTAURATION ---
+router.put("/marqueurs/:marqueurId/archive", isAuth, marqueurController.archiveMarqueur);
+router.put("/marqueurs/:marqueurId/restore", isAuth, marqueurController.restoreMarqueur);
+router.delete("/marqueurs/:marqueurId/permanent", isAuth, marqueurController.deletePermanent);
+
 // POST => /marqueurs/:marqueurId/commentaires
 router.post("/marqueurs/:marqueurId/commentaires", marqueurController.addCommentMarqueur);
 
@@ -38,23 +42,11 @@ router.patch("/marqueurs/:marqueurId/commentaires/:commentId/status", isAuth, ma
 // DELETE => /marqueurs/:marqueurId/commentaires/commentsId
 router.delete("/marqueurs/:marqueurId/commentaires/:commentId", marqueurController.deleteCommentMarqueur);
 
-// DELETE => /marqueurs/:marqueurId
-router.delete("/marqueurs/:marqueurId", isAuth, marqueurController.deleteMarqueur);
-
 // POST => /marqueurs/:marqueurId/edit-requests
 router.post(
   "/marqueurs/:marqueurId/edit-requests",
-  optionalAuth, // ou isAuth si tu veux obliger la connexion
+  optionalAuth,
   editRequestController.createEditRequest
 );
-
-// PUT => /marqueurs/:marqueurId/archive (archiver un marqueur)
-router.put("/marqueurs/:marqueurId/archive", isAuth, marqueurController.archiveMarqueur);
-
-// PUT => /marqueurs/:marqueurId/restore (restaurer un marqueur)
-router.put("/marqueurs/:marqueurId/restore", isAuth, marqueurController.restoreMarqueur);
-
-// DELETE => /marqueurs/:marqueurId/permanent (supprimer définitivement)
-router.delete("/marqueurs/:marqueurId/permanent", isAuth, marqueurController.deletePermanent);
 
 module.exports = router;
