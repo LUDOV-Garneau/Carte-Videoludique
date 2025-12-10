dcf1f4007b16a75ce32ce53e754366b705035a2f
FrederickBlouin <frederick.blouin.5@gmail.com>
2025-12-10 16:36:40 -0500

    Mise à jour du useCommentRequestStore

--- public/src/stores/useCommentRequestStore.js
+++ public/src/stores/useCommentRequestStore.js
@@ -6,6 +6,7 @@ import { useAuthStore } from "./auth";
 export const useCommentRequestStore = defineStore("commentRequestStore", () => {
 
     const pendingComments = ref([]);
+    const archivedComments = ref([]);
     const authStore = useAuthStore();
 
     async function getPendingComments() {
@@ -45,10 +46,53 @@ export const useCommentRequestStore = defineStore("commentRequestStore", () => {
         pendingComments.value = pendingComments.value.filter(c => c.commentId !== idCommentaire);
     }
 
+    /* -----------------------
+       ⭐ COMMENTAIRES ARCHIVÉS
+    ------------------------ */
+    async function getArchivedComments() {
+        const headers = { "Content-Type": "application/json" };
+        if (authStore.token) headers.Authorization = "Bearer " + authStore.token;
+
+        const res = await fetch(`${API_URL}/commentaires/archived`, { headers });
+        const json = await res.json();
+
+        archivedComments.value = json.data;
+        return archivedComments.value;
+    }
+
+    async function restore(marqueurId, commentId) {
+        await fetch(`${API_URL}/marqueurs/${marqueurId}/commentaires/${commentId}/restore`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + authStore.token
+            }
+        });
+
+        archivedComments.value = archivedComments.value.filter(c => c.commentId !== commentId);
+    }
+
+    async function deletePermanent(marqueurId, commentId) {
+        await fetch(`${API_URL}/marqueurs/${marqueurId}/commentaires/${commentId}/definitif`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + authStore.token
+            }
+        });
+
+        archivedComments.value = archivedComments.value.filter(c => c.commentId !== commentId);
+    }
+
     return {
         pendingComments,
         getPendingComments,
         accepter,
-        refuser
+        refuser,
+        // archived
+        archivedComments,
+        getArchivedComments,
+        restore,
+        deletePermanent
     };
 });
